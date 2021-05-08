ce1714e7e55f7316b3b08d1672831596fd9e17a4
devsalwa777 <83880738+devsalwa777@users.noreply.github.com>
2021-05-09 00:00:31 +0530

    Update game.js

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).touchstart(function() {
+$(document).on('touchstart',function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
